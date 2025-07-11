15aeee836b4a36f0f41433ff5bebd031e834ccc1
kethsaxena <praketasaxena@gmail.com>
2025-07-11 13:25:40 -0400

    Downgrade from scala 2.12 to 2.13

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -1,6 +1,19 @@
 package com.example
+import org.apache.spark.sql.SparkSession
 
 object Main extends App {
-  println("Hello from Scala + Maven!")
+
+val spark = SparkSession.builder()
+  .appName("Read CSV Example")
+  .master("local[*]")
+  .getOrCreate()
+
+val df = spark.read
+  .option("header", "true") // true if CSV has headers
+  .option("inferSchema", "true")
+  .csv("/opt/data/Fire_Department_and_Emergency_Medical_Services_Dispatched_Calls_for_Service_20240818.csv")
+
+df.show()
+
 }
 
